0909c0a2d4dc0094fb1160b123065e48bea092ae
Pepestar <mateusmartinez1@hotmail.com>
2022-12-30 00:52:09 -0300

    relogio

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+var int = window.setInterval(() => {carregar()},1000);
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours() + ':' + data.getMinutes()
-    
-    const relogio = setInterval(carregar,1)
+    var hour = new Date
+    var ho = hour.toLocaleTimeString()
+    var h = hour.getHours()
+    msg.innerText = `Agora são ${ho}.`
 
-     msg.innerHTML = `Agora são: ${hora} horas.`
-
-
-if(hora >= 0 && hora < 12 ) {
+if(h >= 0  && h < 12 ) {
     // BOM DIA!
     img.src = 'fotomanha.jpeg'
     document.body.style.background = "#e2cd9f"
-} else if (hora >= 12 && hora <= 18) {
+} else if (h >= 12 && h <= 18) {
     //BoA TARDE!
     img.src = 'fototarde.jpeg'
     document.body.style.background = "#6b84ad"
@@ -24,3 +22,7 @@ if(hora >= 0 && hora < 12 ) {
 }
 
 }
+
+
+
+
